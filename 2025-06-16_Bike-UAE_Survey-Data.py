7fdddc1f00ef72c91787cb78375aef9b334d968c
Max Moran <moranmaxb@gmail.com>
2025-06-17 20:27:31 -0500

    intial duration and progress filtering

--- 2025-06-16_Bike-UAE_Survey-Data.py
+++ 2025-06-16_Bike-UAE_Survey-Data.py
@@ -1,19 +1,23 @@
 ### 2025-06-16_Bike-UAE_Survey-Data
 ### Max Moran
 
-# Set Working Directory to folder with data
+# Import libraries
 import os
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Set Working Directory to folder with data
 path = r"C:\Users\might\Documents\UTSA\Summer_2025\STA-6943_Internship\bike-UAE" #r in front is for the back slashes
 os.chdir(path)
 
-import pandas as pd
-
 # Import data
 df = pd.read_csv('LABELS-UAE Bicycle & Scooter Survey - CITIES_June 16, 2025_16.12.csv')
+print(df.head())
 
 # Drop unnecessary rows
-print(df.head())
 df = df.drop([0,1])
+print("Dataframe after dropping unecessary context rows:")
 print(df.head())
 
 # List of new column names
@@ -71,4 +75,56 @@ new_column_names = [# Automatically generated survey data
 
 # Assign the list to the DataFrame
 df.columns = new_column_names
+print("Dataframe with better column names:")
 print(df.head())
+
+# Convert duration to int then minutes
+df['Duration'] = df['Duration'].astype(int)
+df['Duration'] = df['Duration'] / 60
+
+# Convert progress to int
+df['Progress'] = df['Progress'].astype(int)
+
+
+
+### Handle spam
+# 38 questions for 100% completion, assume ~3 seconds a question; that's 114 seconds (~2 minutes)
+# So, any entries with 100% completion in under 2 minutes we'll be removed (likely spam through questions)
+df = df[~((df['Duration'] < 2) & (df['Progress'] == 100))]
+print("Dataframe without 100% entries under 2 minutes:")
+print(df)
+
+
+
+### Duration Distribution
+# Examine initial boxplot
+sns.boxplot(x=df['Duration'])
+plt.title('Initial Survey Duration Box Plot')
+plt.xlabel('Duration (minutes)')
+plt.show()
+
+# Find Quantiles
+Q1 = df['Duration'].quantile(0.25)
+Q3 = df['Duration'].quantile(0.75)
+IQR = Q3 - Q1
+
+# Define bounds
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+# Filter the DataFrame
+dfDurationIQR = df[(df['Duration'] >= lower_bound) & (df['Duration'] <= upper_bound)]
+print("Removed",len(df['Duration'])-len(dfDurationIQR['Duration']),"outliers")
+
+sns.boxplot(x=dfDurationIQR['Duration'])
+plt.title('Narrowed Survey Duration Box Plot')
+plt.xlabel('Duration (minutes)')
+plt.show()
+
+dfDurationIQR["Duration"].hist(bins=100)
+#plt.ylim(0, 20)
+#plt.xlim(0, 60)
+plt.xlabel('Duration (minutes)')
+plt.ylabel('Number of Respondents')
+plt.title('Survey Completion Time Distribution')
+plt.show()
